150e8a915bb7f26c857d65a274eb75da2234f89c
JoJoJoDoDoDo <itsoref2028@kehillahstudent.org>
2026-01-15 09:10:25 -0800

    yepp

--- main.py
+++ main.py
@@ -1,4 +1,44 @@
-# the user will choose a number
-# this program will guess the number
-# the user will say "yes" or "higher" or "lower"
-# the program will ask to play again or exit
+# # the user will choose a number
+# # this program will guess the number
+# # the user will say "yes" or "higher" or "lower"
+# # the program will ask to play again or exit
+
+# import random
+
+# i = 0
+
+
+
+# while True:
+#     if i == 10:
+#         break
+#     i = i + 1
+
+#     if i == 4:
+#         print("Correct!")
+
+#     else:
+#         print("Wrong!")
+
+
+# input("Is your number 3?")
+
+response = input("Is your number 2?")
+while response == "no":
+    response2  =  (input(f"Is it higher or lower?"))
+while response2 == "lower":
+        (input(f"Is your number 1?"))
+# response3 = (input(f"yes"))
+#     print("Yay!")
+
+        
+while response2 == "higher":
+        (input(f"Is your number 3?"))
+
+
+
+
+while response == "yes":
+    print("Yay!")
+    break
+
